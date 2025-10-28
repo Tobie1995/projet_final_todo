c032ee6c6404eb06ce521bd6993f7c5f73f6a1f5
Tobie <tobie.m@h2altitude.com>
2025-10-28 16:19:20 +0100

    Feat: Finalisation des opérations CRUD et ajout du style

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('taches/', include('taches.urls', namespace='taches')),
+    # Redirect root URL to the tasks HTML list view
+    path('', RedirectView.as_view(pattern_name='taches:liste_html', permanent=False)),
 ]
--- taches/tests.py
+++ taches/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Tache
+
+
+class TacheViewTests(TestCase):
+	def test_tache_list_html_renders(self):
+		"""The HTML task list page should load successfully (HTTP 200)."""
+		url = reverse('taches:liste_html')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+	def test_create_tache_via_post_redirects_and_creates(self):
+		"""POSTing the create form should create a Tache and redirect to the list."""
+		url = reverse('taches:ajouter')
+		data = {
+			'titre': 'Nouvelle tâche de test',
+			'description': 'Description de test',
+			# 'termine' omitted -> should default to False
+		}
+		response = self.client.post(url, data)
+
+		# Should redirect to the list page after successful create
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response.url, reverse('taches:liste_html'))
+
+		# The task should exist in the database
+		self.assertTrue(Tache.objects.filter(titre='Nouvelle tâche de test').exists())
+
